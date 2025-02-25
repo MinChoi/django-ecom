72c7b80c6a4f0f8ab02fca1945e456bd0e972fe3
Min Choi <minc.low@gmail.com>
2025-02-25 14:26:57 +1100

    Add models, customer, category, orders, product

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+
+from django.conf.global_settings import MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
