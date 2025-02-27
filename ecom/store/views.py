11c427dfe14a41cbdfef23fe62bd8d73bbe04c25
Min Choi <minc.low@gmail.com>
2025-02-27 23:26:02 +1100

    registration

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -3,7 +3,10 @@ from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from pprint import pprint
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -37,3 +40,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('Logout done!'))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Registered done!'))
+            return redirect('home')
+        else:
+            messages.warning(request, ('Register fail!'))
+
+    return render(request, 'register.html', {'form':form})
