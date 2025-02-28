bb850523991771fd033e196b943cb8641b1c217c
Min Choi <minc.low@gmail.com>
2025-02-28 11:58:26 +1100

    set category links

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from pprint import pprint
@@ -57,8 +57,21 @@ def register_user(request):
         else:
             messages.warning(request, ('Register fail!'))
 
-    return render(request, 'register.html', {'form':form})
 
 def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
+
+def category(request, category_name):
+    category_name = category_name.replace('-', ' ')
+
+    try:
+        category_selected = Category.objects.get(name=category_name)
+        products = Product.objects.filter(category= category_selected)
+        return render(request, 'category.html', {'products':products})
+    except:
+        messages.warning(request, ('Category doesn\'t exist!'))
+        return redirect('home')
+
     product = Product.objects.get(id=pk)
     return render(request, 'product.html', {'product':product})
