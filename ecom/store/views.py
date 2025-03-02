64863ef8409977a6a8b59ac913935a1e626eeca5
Min Choi <minc.low@gmail.com>
2025-03-02 20:43:58 +1100

    update profile

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from pprint import pprint
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
 
 def home(request):
@@ -83,3 +83,17 @@ def category_summary(request):
 
     return render(request, 'category_summary.html', {'categories':categories})
 
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+            login(request, current_user)
+            messages.success(request, ('Profile updated!'))
+            return redirect('home')
+        return render(request, 'update_user.html', {'user_form':user_form})
+    else:
+        messages.warning(request, ('Login first!'))
+        return redirect('home')
