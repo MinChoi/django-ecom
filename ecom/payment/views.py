28af8251d681c3a848a97bdf6f30ae709eaf664e
Min Choi <minc.low@gmail.com>
2025-03-05 13:42:07 +1100

    billing info

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from .form import ShippingForm
+from .form import ShippingForm, PaymentForm
 from .models import ShippingAddress
+
+
+
+def billing_info(request):
+
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods()
+        quantities = cart.get_quantities()
+        total = cart.cart_total()
+
+        # check if user is logged in
+        if request.user.is_authenticated:
+            billing_form = PaymentForm
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities,
+                                                             'total': total, 'shipping_info': request.POST, 'billing_form':billing_form})
+        else:
+            billing_form = PaymentForm
+            return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities,
+                                                             'total': total, 'shipping_info': request.POST, 'billing_form':billing_form})
+
+        shipping_info = request.POST
+        return render(request, 'payment/billing_info.html', {'cart_products':cart_products, 'quantities':quantities,
+                                                      'total': total, 'shipping_info': request.POST})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
 
 
 def checkout(request):
@@ -15,11 +43,13 @@ def checkout(request):
         shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
 
         shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
-        return render(request, 'payment/checkout.html', {'cart_products':cart_products, 'quantities':quantities, 'total':total})
+        return render(request, 'payment/checkout.html', {'cart_products':cart_products, 'quantities':quantities,
+                                                         'total':total, 'shipping_form':shipping_form})
     else:
         # guess
         shipping_form = ShippingForm(request.POST or None)
-        return render(request, 'payment/checkout.html', {'cart_products':cart_products, 'quantities':quantities, 'total':total})
+        return render(request, 'payment/checkout.html', {'cart_products':cart_products, 'quantities':quantities,
+                                                         'total':total, 'shipping_form':shipping_form})
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
